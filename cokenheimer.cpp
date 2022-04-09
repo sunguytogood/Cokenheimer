db9ad2612f7d60f7692ff45b5d8b293478550ea2
sunguytogood <60748152+sunguytogood@users.noreply.github.com>
2022-04-09 20:04:17 +0200

    Create cokenheimer.cpp

---
+++ cokenheimer.cpp
@@ -0,0 +1,24 @@
+import rickroll
+import yourmom
+import fuck
+import popbob
+import all the above 69 times
+from popbob add thefunny
+
+thefunny.scan(port:25565)
+  range = 10.10.10.10 - 255.255.255.255
+  
+  db.json = file.open()
+  db2.json = file.open2()
+ login.Minecraft(User:"Peter", Pass: "Peterbob")
+    fuck.check(status)
+  if (whitelist) == false
+    if version == 1.17
+      if state == vanilla or cracked
+    file.write("the good shit" whitelist + version + state)
+        if else
+          file.write2("regular shit")
+    
+          
+ 
+
